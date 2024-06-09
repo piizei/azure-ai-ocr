18bbe7d3a1df0a41d257f44252c8d64e7912853e
petjohansson <191493+piizei@users.noreply.github.com>
2024-06-09 10:23:47 +0200

    support office docs

--- ai_ocr/azure/doc_intelligence.py
+++ ai_ocr/azure/doc_intelligence.py
@@ -17,6 +17,5 @@ def get_ocr_results(file_path: str, output: str = "markdown") -> AnalyzeResult:
         poller = client.begin_analyze_document("prebuilt-layout",
                                                analyze_request=f,
                                                content_type="application/octet-stream",
-                                               features=["keyValuePairs"],
                                                output_content_format=output)
     return poller.result()
